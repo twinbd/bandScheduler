883e74e2629174fa382cb380fe14aa852aa35130
Jaeyun Jeong <jaeyunjeong101@gmail.com>
2024-09-26 21:19:39 -0700

    Add existing project files prior to the push to GitHub.

---
+++ server/routes/Users.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const router = express.Router();
+const { Users } = require("../models");
+const bcrypt = require("bcrypt");
+const { validateToken } = require("../middlewares/AuthMiddleware");
+
+const { sign } = require("jsonwebtoken");
+
+// registration
+router.post("/", async (req, res) => {
+  const { username, password } = req.body;
+  bcrypt.hash(password, 10).then((hash) => {
+    Users.create({
+      username: username,
+      password: hash,
+    });
+    res.json("Success");
+  });
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await Users.findOne({ where: { username: username } });
+  if (!user) return res.json({ error: "User doesn't exist" });
+
+  bcrypt.compare(password, user.password).then((match) => {
+    if (!match)
+      return res.json({ error: "Wrong username and password combination" });
+
+    const accessToken = sign(
+      { username: user.username, id: user.id },
+      "importantsecret"
+    );
+    return res.json({ token: accessToken, username: username, id: user.id });
+  });
+});
+
+router.get("/auth", validateToken, (req, res) => {
+  res.json(req.user);
+});
+
+router.get("/basicinfo/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const basicInfo = await Users.findByPk(id, {
+    attributes: { exclude: ["password"] },
+  });
+  res.json(basicInfo);
+});
+
+router.put("/changepassword", validateToken, async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+  const user = await Users.findOne({ where: { username: req.user.username } });
+
+  bcrypt.compare(oldPassword, user.password).then((match) => {
+    if (!match) {
+      return res.json({ error: "Wrong old password entered" });
+    }
+
+    bcrypt.hash(newPassword, 10).then((hash) => {
+      Users.update(
+        { password: hash },
+        { where: { username: req.user.username } }
+      );
+      res.json("Success");
+    });
+  });
+});
+
+module.exports = router;
